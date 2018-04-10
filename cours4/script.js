ea7d7d95703fe90610b56dae40c54a8316b1511a
NATACHA YU <94natacha@live.fr>
2018-04-10 11:35:56 +0200

    cours 4 - défis

---
+++ cours4/script.js
@@ -0,0 +1,11 @@
+
+$( document ).ready(function() {
+ 
+  function welcome() {
+   $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
+
+}; 
+
+welcome();
+
+})
